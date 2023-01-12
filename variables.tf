23ed7acd310cb45c5300cc61e1b59e5f1731e1d9
aksharbarot <axar1990@gmail.com>
2023-01-12 14:25:26 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,10 @@ variable "aws_az" {
   type        = string
   description = "The AWS region 1 ."
 }
+
+
+variable events {
+  default     = ["ASSESSMENT_RUN_STARTED", "ASSESSMENT_RUN_COMPLETED", "ASSESSMENT_RUN_STATE_CHANGED", "FINDING_REPORTED"]
+  description = "A list of events to fire to an SNS topic. May contain ASSESSMENT_RUN_STARTED, ASSESSMENT_RUN_COMPLETED, ASSESSMENT_RUN_STATE_CHANGED, and/or FINDING_REPORTED."
+  type        = list(string)
+}
